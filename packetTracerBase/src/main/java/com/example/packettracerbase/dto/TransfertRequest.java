7158a5a093f3bae9a8325c0503f59097a7f2a4e5
Yassine_K <kacimiyassine16@gmail.com>
2024-05-23 22:52:19 +0100

    dddddddddddddddddVYTGVHGV

---
+++ packetTracerBase/src/main/java/com/example/packettracerbase/dto/TransfertRequest.java
@@ -0,0 +1,12 @@
+package com.example.packettracerbase.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class TransfertRequest {
+    private Long codeSecteur;
+    private Long idDriver;
+
+}
