0e8ff021bdd42079e50912c646ccd5dbc1c9d3ba
Yassine_K <kacimiyassine16@gmail.com>
2024-05-23 22:51:18 +0100

    vvvvvvvvvvvvvvvvvvvvvHGCVGHChhhhhhhhhhhhhhhh

---
+++ PacketTracer/app/src/main/java/com/example/packettracer/model/TransfertRequest.java
@@ -0,0 +1,22 @@
+package com.example.packettracer.model;
+
+public class TransfertRequest {
+    private Long codeSecteur;
+    private Long idDriver;
+
+    public Long getCodeSecteur() {
+        return codeSecteur;
+    }
+
+    public void setCodeSecteur(Long codeSecteur) {
+        this.codeSecteur = codeSecteur;
+    }
+
+    public Long getIdDriver() {
+        return idDriver;
+    }
+
+    public void setIdDriver(Long idDriver) {
+        this.idDriver = idDriver;
+    }
+}
